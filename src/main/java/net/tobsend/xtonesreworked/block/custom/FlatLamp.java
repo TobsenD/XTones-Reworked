e1303b496e6e4996a34cac1c9cc53613487d7808
TobsenD <tobias.dols@outlook.com>
2022-10-27 00:06:21 +0200

    Arranging Model for placement position

--- src/main/java/net/tobsend/xtonesreworked/block/custom/FlatLamp.java
+++ src/main/java/net/tobsend/xtonesreworked/block/custom/FlatLamp.java
@@ -1,20 +1,43 @@
 package net.tobsend.xtonesreworked.block.custom;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.RedstoneLampBlock;
+import net.minecraft.world.level.block.Mirror;
+import net.minecraft.world.level.block.Rotation;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.BooleanProperty;
+import net.minecraft.world.level.block.state.properties.DirectionProperty;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
 
-public class FlatLamp extends RedstoneLampBlock {
+public class FlatLamp extends Block {
 
+  public static final BooleanProperty LIT = BooleanProperty.create("lit");
+  private static final DirectionProperty FACING = BlockStateProperties.FACING;
   private static final VoxelShape SHAPE = Block.box(0, 0, 0, 16, 3, 16);
 
   public FlatLamp(Properties properties) {
     super(properties);
+    this.registerDefaultState(this.defaultBlockState().setValue(LIT, Boolean.valueOf(false)));
   }
+
+  @Override
+  public BlockState rotate(BlockState blockState, Rotation rotation) {
+    return blockState.setValue(FACING, rotation.rotate(blockState.getValue(FACING)));
+ }
+
+ public BlockState mirror(BlockState blockState, Mirror mirror) {
+    return blockState.rotate(mirror.getRotation(blockState.getValue(FACING)));
+ }
+
+
+ public BlockState getStateForPlacement(BlockPlaceContext blockPlaceContext) {
+    return this.defaultBlockState().setValue(FACING, blockPlaceContext.getNearestLookingDirection().getOpposite());
+ }
 
   @Override
   public VoxelShape getShape(
@@ -24,5 +47,10 @@ public class FlatLamp extends RedstoneLampBlock {
     CollisionContext collisionContext
   ) {
     return SHAPE;
+  }
+
+  @Override
+  protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+      builder.add(FACING, LIT);
   }
 }
