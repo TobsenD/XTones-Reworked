cb9fa0d37ea813230da257fbaee9ee0059fe6a20
TobsenD <tobias.dols@outlook.com>
2022-10-26 15:04:52 +0200

    Added Azur Blocks

--- src/main/java/net/tobsend/xtonesreworked/XtonesReworkedMod.java
+++ src/main/java/net/tobsend/xtonesreworked/XtonesReworkedMod.java
@@ -9,6 +9,7 @@ import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.tobsend.xtonesreworked.block.AgonBlocks;
+import net.tobsend.xtonesreworked.block.AzurBlocks;
 import net.tobsend.xtonesreworked.block.ModBlocks;
 import net.tobsend.xtonesreworked.item.ModItems;
 import org.slf4j.Logger;
@@ -27,6 +28,7 @@ public class XtonesReworkedMod {
     ModItems.register(modEventBus);
     ModBlocks.register(modEventBus);
     AgonBlocks.register(modEventBus);
+    AzurBlocks.register(modEventBus);
 
     modEventBus.addListener(this::commonSetup);
     MinecraftForge.EVENT_BUS.register(this);
---
+++ src/main/java/net/tobsend/xtonesreworked/block/AzurBlocks.java
@@ -0,0 +1,231 @@
+package net.tobsend.xtonesreworked.block;
+
+
+import java.util.function.Supplier;
+
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.material.Material;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import net.tobsend.xtonesreworked.XtonesReworkedMod;
+import net.tobsend.xtonesreworked.item.ModCreativeModeTab;
+import net.tobsend.xtonesreworked.item.ModItems;
+
+public class AzurBlocks {
+
+  private static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(
+    ForgeRegistries.BLOCKS,
+    XtonesReworkedMod.MOD_ID
+  );
+
+  public static final RegistryObject<Block> AZUR_BLOCK_0 = registerBlock(
+    "azur_block_0",
+    () ->
+      new Block(
+        BlockBehaviour.Properties
+          .of(Material.STONE)
+          .strength(6f)
+          .requiresCorrectToolForDrops()
+      ),
+    ModCreativeModeTab.XTONE_REWORK_TAB
+  );
+
+  public static final RegistryObject<Block> AZUR_BLOCK_1 = registerBlock(
+    "azur_block_1",
+    () ->
+      new Block(
+        BlockBehaviour.Properties
+          .of(Material.STONE)
+          .strength(6f)
+          .requiresCorrectToolForDrops()
+      ),
+    ModCreativeModeTab.XTONE_REWORK_TAB
+  );
+  public static final RegistryObject<Block> AZUR_BLOCK_2 = registerBlock(
+    "azur_block_2",
+    () ->
+      new Block(
+        BlockBehaviour.Properties
+          .of(Material.STONE)
+          .strength(6f)
+          .requiresCorrectToolForDrops()
+      ),
+    ModCreativeModeTab.XTONE_REWORK_TAB
+  );
+  public static final RegistryObject<Block> AZUR_BLOCK_3 = registerBlock(
+    "azur_block_3",
+    () ->
+      new Block(
+        BlockBehaviour.Properties
+          .of(Material.STONE)
+          .strength(6f)
+          .requiresCorrectToolForDrops()
+      ),
+    ModCreativeModeTab.XTONE_REWORK_TAB
+  );
+  public static final RegistryObject<Block> AZUR_BLOCK_4 = registerBlock(
+    "azur_block_4",
+    () ->
+      new Block(
+        BlockBehaviour.Properties
+          .of(Material.STONE)
+          .strength(6f)
+          .requiresCorrectToolForDrops()
+      ),
+    ModCreativeModeTab.XTONE_REWORK_TAB
+  );
+  public static final RegistryObject<Block> AZUR_BLOCK_5 = registerBlock(
+    "azur_block_5",
+    () ->
+      new Block(
+        BlockBehaviour.Properties
+          .of(Material.STONE)
+          .strength(6f)
+          .requiresCorrectToolForDrops()
+      ),
+    ModCreativeModeTab.XTONE_REWORK_TAB
+  );
+  public static final RegistryObject<Block> AZUR_BLOCK_6 = registerBlock(
+    "azur_block_6",
+    () ->
+      new Block(
+        BlockBehaviour.Properties
+          .of(Material.STONE)
+          .strength(6f)
+          .requiresCorrectToolForDrops()
+      ),
+    ModCreativeModeTab.XTONE_REWORK_TAB
+  );
+
+  public static final RegistryObject<Block> AZUR_BLOCK_7 = registerBlock(
+    "azur_block_7",
+    () ->
+      new Block(
+        BlockBehaviour.Properties
+          .of(Material.STONE)
+          .strength(6f)
+          .requiresCorrectToolForDrops()
+      ),
+    ModCreativeModeTab.XTONE_REWORK_TAB
+  );
+  public static final RegistryObject<Block> AZUR_BLOCK_8 = registerBlock(
+    "azur_block_8",
+    () ->
+      new Block(
+        BlockBehaviour.Properties
+          .of(Material.STONE)
+          .strength(6f)
+          .requiresCorrectToolForDrops()
+      ),
+    ModCreativeModeTab.XTONE_REWORK_TAB
+  );
+  public static final RegistryObject<Block> AZUR_BLOCK_9 = registerBlock(
+    "azur_block_9",
+    () ->
+      new Block(
+        BlockBehaviour.Properties
+          .of(Material.STONE)
+          .strength(6f)
+          .requiresCorrectToolForDrops()
+      ),
+    ModCreativeModeTab.XTONE_REWORK_TAB
+  );
+  public static final RegistryObject<Block> AZUR_BLOCK_10 = registerBlock(
+    "azur_block_10",
+    () ->
+      new Block(
+        BlockBehaviour.Properties
+          .of(Material.STONE)
+          .strength(6f)
+          .requiresCorrectToolForDrops()
+      ),
+    ModCreativeModeTab.XTONE_REWORK_TAB
+  );
+  public static final RegistryObject<Block> AZUR_BLOCK_11 = registerBlock(
+    "azur_block_11",
+    () ->
+      new Block(
+        BlockBehaviour.Properties
+          .of(Material.STONE)
+          .strength(6f)
+          .requiresCorrectToolForDrops()
+      ),
+    ModCreativeModeTab.XTONE_REWORK_TAB
+  );
+  public static final RegistryObject<Block> AZUR_BLOCK_12 = registerBlock(
+    "azur_block_12",
+    () ->
+      new Block(
+        BlockBehaviour.Properties
+          .of(Material.STONE)
+          .strength(6f)
+          .requiresCorrectToolForDrops()
+      ),
+    ModCreativeModeTab.XTONE_REWORK_TAB
+  );
+  public static final RegistryObject<Block> AZUR_BLOCK_13 = registerBlock(
+    "azur_block_13",
+    () ->
+      new Block(
+        BlockBehaviour.Properties
+          .of(Material.STONE)
+          .strength(6f)
+          .requiresCorrectToolForDrops()
+      ),
+    ModCreativeModeTab.XTONE_REWORK_TAB
+  );
+  public static final RegistryObject<Block> AZUR_BLOCK_14 = registerBlock(
+    "azur_block_14",
+    () ->
+      new Block(
+        BlockBehaviour.Properties
+          .of(Material.STONE)
+          .strength(6f)
+          .requiresCorrectToolForDrops()
+      ),
+    ModCreativeModeTab.XTONE_REWORK_TAB
+  );
+  public static final RegistryObject<Block> AZUR_BLOCK_15 = registerBlock(
+    "azur_block_15",
+    () ->
+      new Block(
+        BlockBehaviour.Properties
+          .of(Material.STONE)
+          .strength(6f)
+          .requiresCorrectToolForDrops()
+      ),
+    ModCreativeModeTab.XTONE_REWORK_TAB
+  );
+
+  private static <T extends Block> RegistryObject<T> registerBlock(
+    String name,
+    Supplier<T> block,
+    CreativeModeTab tab
+  ) {
+    RegistryObject<T> toReturn = BLOCKS.register(name, block);
+    registerBlockItem(name, toReturn, tab);
+    return toReturn;
+  }
+
+  private static <T extends Block> RegistryObject<Item> registerBlockItem(
+    String name,
+    RegistryObject<T> block,
+    CreativeModeTab tab
+  ) {
+    return ModItems.ITEMS.register(
+      name,
+      () -> new BlockItem(block.get(), new Item.Properties().tab(tab))
+    );
+  }
+
+  public static void register(IEventBus eventBus) {
+    BLOCKS.register(eventBus);
+  }
+
+}
